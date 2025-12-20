b28fad2d9241c224e145accf8c71039db6035508
Filipe Larga <filipe.larga@premium-minds.com>
2025-12-20 21:14:52 +0000

    Add template day

---
+++ src/bin/day_xx.rs
@@ -0,0 +1,17 @@
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("inputs/day_xx.txt").expect("Failed to read input");
+    let input = r#""#;
+
+    println!("Part 1: {}", part1());
+    println!("Part 2: {}", part2());
+}
+
+fn part1() -> usize {
+    0
+}
+
+fn part2() -> usize {
+    0
+}
